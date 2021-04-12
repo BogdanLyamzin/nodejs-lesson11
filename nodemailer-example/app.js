37634bfecd4ec8d2bb176ae43481daf37aa78327
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-04-12 19:22:25 +0300

    add new lesson

---
+++ nodemailer-example/app.js
@@ -0,0 +1,10 @@
+
+
+const transporter = nodemailer.createTransport(transporterConfig);
+
+// устанавливаем параметры письма
+
+/*
+SMTP, POP3, IMAP сервер:	mail.adm.tools
+Порт SMTP сервера:	25 или 2525 или 465 (SMTP+SSL)
+*/
---
+++ sendgrid-example/app.js
@@ -0,0 +1,3 @@
+
+
+// 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
